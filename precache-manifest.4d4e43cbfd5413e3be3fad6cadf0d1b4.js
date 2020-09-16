5acb0551d15302cd90cf9259c44bafae97cb9da5
anvesh balaji <anvesh.b@globeshout.com>
2020-09-16 16:52:57 +0530

    Updates

---
+++ precache-manifest.4d4e43cbfd5413e3be3fad6cadf0d1b4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e717d6a850f3145ac59008de3dd0faeb",
+    "url": "/infinity_web/index.html"
+  },
+  {
+    "revision": "0947c34abbcbf1804fff",
+    "url": "/infinity_web/static/css/2.68dd0589.chunk.css"
+  },
+  {
+    "revision": "99f61531f53498c59f25",
+    "url": "/infinity_web/static/css/main.542b27d4.chunk.css"
+  },
+  {
+    "revision": "0947c34abbcbf1804fff",
+    "url": "/infinity_web/static/js/2.f438af37.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/infinity_web/static/js/2.f438af37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99f61531f53498c59f25",
+    "url": "/infinity_web/static/js/main.bc6056f7.chunk.js"
+  },
+  {
+    "revision": "0c341392aa59971de935",
+    "url": "/infinity_web/static/js/runtime-main.d37bd453.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/infinity_web/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/infinity_web/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/infinity_web/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/infinity_web/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  }
+]);
